56e31253202878370b4c619dd78cd95fe1bd611c
ntzolov <ntzolov@gmail.com>
2023-11-29 18:29:29 +0200

    Problem solved

---
+++ TS/5 kyu/ISBN-10 Validation.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.validISBN10 = void 0;
+function validISBN10(isbn) {
+    const isInvalid = new RegExp(/[^0123456789X]/g);
+    const values = isbn.split('');
+    if (isInvalid.test(isbn) || values.length !== 10) {
+        return false;
+    }
+    let total = 0;
+    values.forEach((value, index) => {
+        if (value === 'X' && index !== 9) {
+            return false;
+        }
+        if (value === 'X') {
+            total += (index + 1) * 10;
+            return;
+        }
+        total += (index + 1) * Number(value);
+    });
+    return Boolean(!(total % 11));
+}
+exports.validISBN10 = validISBN10;
+validISBN10('X123456788');
+//# sourceMappingURL=ISBN-10%20Validation.js.map
---
+++ TS/5 kyu/ISBN-10 Validation.ts
@@ -0,0 +1,27 @@
+export function validISBN10(isbn: string): boolean {
+  const isInvalid = new RegExp(/[^0123456789X]/g)
+  const values = isbn.split('')
+
+  if (isInvalid.test(isbn) || values.length !== 10) {
+    return false
+  }
+
+  let total = 0
+
+  values.forEach((value, index) => {
+    if (value === 'X' && index !== 9) {
+      return false
+    }
+
+    if (value === 'X') {
+      total += (index + 1) * 10
+      return
+    }
+
+    total += (index + 1) * Number(value)
+  })
+
+  return Boolean(!(total % 11))
+}
+
+validISBN10('X123456788')
